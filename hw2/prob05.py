3b4e7d7d4f0b3492396e2b98aa5369a55f0f3608
GPL <glinkowski@users.noreply.github.com>
2016-09-20 22:53:09 -0500

    questionable Housholder algo

--- hw2/prob05.py
+++ hw2/prob05.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as pt
 
 
 
@@ -19,6 +20,7 @@ def createHilbertArray() :
 
 	return hilbert
 #end def ######## ####### #######
+
 
 def origGramSchmidt(B) :
 
@@ -46,6 +48,7 @@ def origGramSchmidt(B) :
 	return Q, R
 #end def ######## ####### #######
 
+
 def modGramSchmidt(B) :
 
 	A = np.copy(B)
@@ -67,6 +70,45 @@ def modGramSchmidt(B) :
 #end def ######## ####### #######
 
 
+def getError(Q) :
+
+	E = -1 * np.log10( np.linalg.norm(np.identity(Q.shape[0]) - np.dot( np.transpose(Q), Q)))
+	E = max(E, 0)
+
+	return E
+#end def ######## ####### #######
+
+
+def Householder(B) :
+
+	A = np.copy(B)
+	(numRows, numCols) = A.shape
+	# print("A")
+	# print(A)
+
+#	H = np.zeros(A.shape, dtype=np.float64)
+	H = np.identity(numCols)
+	e = np.identity(numCols)
+
+	for n in range(numCols) :
+		alpha = np.linalg.norm(A[:,n])
+		# print(alpha)
+		v = A[:,n] - np.multiply(-alpha, e[:,n])
+		vT = v.reshape(len(v), 1)
+#		print(v)
+		H_i = e - np.multiply( (2 / np.dot(v, vT)), np.multiply(vT, v) )
+		# print("H_{}".format(n))
+		# print(H_i)
+		A = np.dot(H_i, A)
+		# print("H_i * A")
+		# print(A)
+		H = np.dot(H_i, H)
+	#end loop
+#	print(H)
+
+	Q = np.transpose(H)
+	return Q
+#end def ######## ####### #######
 
 
 ######## ######## ####### #######
@@ -74,32 +116,77 @@ def modGramSchmidt(B) :
 
 hilbert = createHilbertArray()
 
-testIdx = 2
+testIdx = 6
 
-#print(hilbert[testIdx])
+# #print(hilbert[testIdx])
+
+# Q, R = origGramSchmidt(hilbert[testIdx])
+# print(Q)
+
+# Q, R = modGramSchmidt(hilbert[testIdx])
+# print(Q)
+
+# Q1, R1 = np.linalg.qr(hilbert[testIdx])
+# print(Q1)
+
+# # # Q^T * Q should ~= I
+# print( np.dot( np.transpose(Q), Q ) )
+# # print( np.linalg.norm(np.identity(testIdx + 2) - np.dot( np.transpose(Q), Q)) )
+# # #print(R)
+
+# E = -1 * np.log10( np.linalg.norm(np.identity(testIdx + 2) - np.dot( np.transpose(Q), Q)))
+# print(E)
+
 
 Q, R = origGramSchmidt(hilbert[testIdx])
 print(Q)
-
-Q, R = modGramSchmidt(hilbert[testIdx])
-print(Q)
-
-Q1, R1 = np.linalg.qr(hilbert[testIdx])
-print(Q1)
-
-# # Q^T * Q should ~= I
 print( np.dot( np.transpose(Q), Q ) )
-# print( np.linalg.norm(np.identity(testIdx + 2) - np.dot( np.transpose(Q), Q)) )
-# #print(R)
+print(getError(Q))
 
-E = -1 * np.log10( np.linalg.norm(np.identity(testIdx + 2) - np.dot( np.transpose(Q), Q)))
+Q = Householder(hilbert[testIdx])
+print(Q)
+print( np.dot( np.transpose(Q), Q ) )
+E = getError(Q)
 print(E)
 
 
 
 x = list()
-y_o = list()
-y_m = list()
+y_origGS = list()
+y_modGS = list()
+y_selfGS = list()
+y_HH = list()
 
-#for matrix in hilbert :
+entry = 2
+for matrix in hilbert :
 
+	x.append(entry)
+	entry += 1
+
+	Q, R = origGramSchmidt(matrix)
+	E = getError(Q)
+	y_origGS.append(E)
+
+	Q, R = origGramSchmidt(Q)
+	E = getError(Q)
+	y_selfGS.append(E)
+
+	Q, R = modGramSchmidt(matrix)
+	E = getError(Q)
+	y_modGS.append(E)
+
+	Q = Householder(matrix)
+	E = getError(Q)
+	y_HH.append(E)
+#end loop
+
+# Plot the stuff
+pt.plot(x, y_origGS, x, y_modGS, x, y_selfGS, x, y_HH)
+pt.xlim([0,12])
+pt.xlabel('Hilbert matrix size nxn')
+pt.ylabel('digits of accuracy')
+pt.legend(['classical Gram-Schmidt', 'modified Gram-Schmidt',
+		'Gram-Schmidt on self', 'Householder transform'])
+pt.title('Digits of Accuracy vs Matrix Dimensions')
+pt.savefig('linkows2-hw2Prob5.png')
+pt.show()
