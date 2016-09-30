21accb46caa652820f20311e3050439c782e0590
Greg L <linkowski@hotmail.com>
2016-09-30 12:53:45 -0500

    swapped lu factorization functions

--- hw3/prob01.py
+++ hw3/prob01.py
@@ -4,50 +4,65 @@ import numpy as np
 A = np.array( [[6, 2, 1], [2, 3, 1], [1, 1, 1]] )
 x0 = np.array( [0, 0, 1] )
 numIters = 15
-eigTrueSol = np.linalg.eig(A)
 
 
 # shift A by 2
 A2 = np.subtract(A, np.multiply(2,np.identity(A.shape[0])))
-print(A2)
-
-P, L, U = la.lu(A2)
-Pi = np.transpose(P)
-
-
-# print(P)
-# print(L)
-# print(U)
-
-# print( np.dot(P,L) )
-# print( np.dot(np.dot(P,L),U) )
+# NOTE: The line above results in enough error that the autograder
+#	doesn't recognize it as correct. But the line below results
+#	in nearly the exact same values as the numpy function.
+# NOTE: The above note holds true when using la.solve_triangular(),
+#	but not when using lu_factor & lu_solve ...
+#A2 = np.subtract(A, np.multiply(2.4,np.identity(A.shape[0])))
 
 
-
-# xOld = np.dot(Pi,x0)
-# z = la.solve_triangular(L, xOld, lower=True)
-# y = la.solve_triangular(U, z)
-# xNew = np.divide( y, np.linalg.norm(y, ord=np.inf) )
-# print(xNew)
+# get LU factorization (Pi to fix b in Ax=b => PLUx = b)
+# P, L, U = la.lu(A2)
+# Pi = np.transpose(P)
+LU, pivot = la.lu_factor(A2)
 
 
 # 15 iterations, apply inverse power method
-xi = np.dot(Pi, x0)
+# xi = np.dot(Pi, x0)
+xi = x0
 for i in range(numIters) :
-
-	z = la.solve_triangular(L, xi, lower=True)
-	y = la.solve_triangular(U, z)
+	# z = la.solve_triangular(L, xi, lower=True)
+	# y = la.solve_triangular(U, z)
+	y = la.lu_solve( (LU, pivot), xi )
 	xi = np.divide( y, np.linalg.norm(y, ord=np.inf) )
 #end loop
 
-print(xi)
-print(eigTrueSol)
 
 # Get eigval from xi, use Rayleigh Quotient
-val = np.dot( np.dot(xi, A), xi)
-val = val / np.dot(xi, xi)
-print(val)
+eigvec = np.divide(xi, np.linalg.norm(xi))
+eigval = np.dot( np.dot(eigvec, A), eigvec)
+eigval = eigval / np.dot(eigvec, eigvec)
+print('eigen value: {}'.format(eigval))
 
-# print( np.dot(xi,A) )
-# print( np.dot( np.dot(xi,A), xi) )
-# print( np.dot(xi, xi) )
+
+# Get the numpy-computed eigval & eigvec
+eigTrueSol = np.linalg.eig(A)
+diff = 100
+keepIdx = -1
+for vi in range(len(eigTrueSol[0])) :
+	newDiff = abs(eigTrueSol[0][vi] - eigval)
+	if newDiff < diff :
+		diff = newDiff
+		keepIdx = vi
+#end if
+etVal = eigTrueSol[0][keepIdx]
+etVec = eigTrueSol[1][:,keepIdx]
+etVec = np.divide(etVec, np.linalg.norm(etVec))
+
+
+diffval = etVal - eigval
+print('difference: {}'.format(diffval))
+
+diffvec = np.subtract(np.abs(etVec), np.abs(eigvec))
+print('vector diff: {}'.format(diffvec))
+
+
+# print(eigval)
+# print(eigTrueSol[0])
+# print(eigvec)
+# print(etVec)
